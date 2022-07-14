66a89f1f0fa5b2044e944ebb263e6462887cc66a
PierrotAWB <andrewwang298@gmail.com>
2022-07-14 10:07:52 -0400

    Rebase onto master

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use walkdir::WalkDir;
+
 fn main() {
-    println!("Hello, world!");
+    for entry in WalkDir::new(".") {
+        let entry = entry.unwrap();
+        println!("{}", entry.path().display());
+    }
 }
