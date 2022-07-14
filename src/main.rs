2d64703c8e6584596184ec81c1fe56b420ed54b0
PierrotAWB <andrewwang298@gmail.com>
2022-07-14 11:01:03 -0400

    Walk directory; print pdfs only

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,20 @@ use walkdir::WalkDir;
 fn main() {
     for entry in WalkDir::new(".") {
         let entry = entry.unwrap();
-        println!("{}", entry.path().display());
+        if !entry.path_is_symlink() {
+            let filename = entry.file_name().
+                to_str().
+                expect("Unable to process filename");
+            let parts: Vec<&str> = filename.split('.').collect();
+
+            match parts.last() {
+                Some(v) => match *v {
+                    "pdf" => println!("{}", entry.path().display()),
+                    &_ => ()
+                }
+                None => ()
+            }
+
+        }
     }
 }
